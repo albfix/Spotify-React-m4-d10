bd9ac76437c01d17889afc2edfcdc63346d899a9
Alberto Gaias <63840357+albfix@users.noreply.github.com>
2020-06-12 14:14:56 +0200

    Home.jsx : Render + console.log + return with short circuit * div + change color alert

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -9,9 +9,9 @@ import Home from './components/Home'
 function App() {
   return (
     <div className="App">
-      <Home />
       <SideBar />
       <SideNav />
+      <Home />
     </div>
   );
 }
--- spotify-react/src/components/Home.jsx
+++ spotify-react/src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Container, Alert } from 'react-bootstrap'
 //import { Container, Row, Col } from "react-bootstrap"
 
 const deezApi = "https://deezerdevs-deezer.p.rapidapi.com/search?metallica"
@@ -8,11 +9,11 @@ class Home extends React.Component {
         super(props);
         this.state = {
             albums: [],
-            loading: false
+            loading: false,
+            err: false
             //comments : [] INSERIRE DOPO
         }
     }
-
 
     componentDidMount() {
         fetch(deezApi)
@@ -21,12 +22,45 @@ class Home extends React.Component {
                 this.setState({ albums: responseObject.search })
             )
 
-    }
+    };
 
     render() {
-        //TRY CONSOLE IF COMPONENT WORKS 
-        console.log("rendered")
-        return
+        //METODO RENDER DEI COMPONENTI+CONSOLE.LOG+RETURN WHITH SHORT CIRCUIT+DIV PER GALLERIE
+        console.log("render")
+        return (
+            <Container>
+                {this.state.err && (
+                    <Alert variant="danger">
+                        An error has occurred, please try again later
+                    </Alert>
+                )}
+                {!this.state.error && (
+                    <div>/
+                        <Gallery
+                            title="Harry Potter"
+                            loading={this.state.loading}
+                            movies={this.state.harryPotter.slice(0, 6)}
+                            comments={this.state.comments}
+                            fetchComments={this.fetchComments}
+                        />
+                        <Gallery
+                            title="Spider Man"
+                            loading={this.state.loading}
+                            movies={this.state.spiderMan.slice(0, 6)}
+                            comments={this.state.comments}
+                            fetchComments={this.fetchComments}
+                        />
+                        <Gallery
+                            title="Matrix"
+                            loading={this.state.loading}
+                            movies={this.state.matrix.slice(0, 6)}
+                            comments={this.state.comments}
+                            fetchComments={this.fetchComments}
+                        />
+                    </div>
+                )}
+            </Container>
+        )
     }
 }
 export default Home
